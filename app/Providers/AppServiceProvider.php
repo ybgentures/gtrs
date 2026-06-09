00074f61c11988fb2d6edaaa3ff8760fd2867d4e
ybgentures <yearbookforgentures23@gmail.com>
2026-06-09 16:22:00 +0700

    love u

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // <--- INI BARIS YANG HILANG!
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,8 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        // Memaksa HTTPS jika aplikasi berjalan di environment production
         if ($this->app->environment('production')) {
             URL::forceScheme('https');
-    }   
+        }
     }
 }
