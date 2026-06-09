a9b5008b9d51d26890123bf2ca7df2641c0ddae2
ybgentures <yearbookforgentures23@gmail.com>
2026-06-09 15:39:39 +0700

    you

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -10,6 +10,7 @@
   <title>Gentures</title>
   <link rel="shortcut icon" href="img/log_gen3.webp"/>
   @vite('resources/css/app.css')
+  @vite('resources/js/app.js')
   <link rel="preconnect" href="https://fonts.googleapis.com" />
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
   <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
